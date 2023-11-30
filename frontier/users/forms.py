9b93d3a9614b7c9743497b39b02055600982bea4
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-11-30 15:10:44 +0400

    Commit before big reconfiguration

--- frontier/users/forms.py
+++ frontier/users/forms.py
@@ -1,7 +1,49 @@
 import django.forms as forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(label='Имя пользователя', widget=forms.TextInput())
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+
+
+class UserForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'type': 'username',
+            'placeholder': 'Username'
+        }
+    ))
+    first_name = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'type': 'first_name',
+            'placeholder': 'First Name'
+        }
+    ))
+    last_name = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'type': 'last_name',
+            'placeholder': 'Last Name'
+        }
+    ))
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={
+            'type': 'email',
+            'placeholder': 'E-mail'
+        }
+    ))
+    password1 = forms.CharField(max_length=16, widget=forms.PasswordInput(
+        attrs={
+            'placeholder': 'Password'
+        }
+    ))
+    password2 = forms.CharField(max_length=16, widget=forms.PasswordInput(
+        attrs={
+            'placeholder': 'Repeat Password'
+        }
+    ))
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
--- frontier/users/urls.py
+++ frontier/users/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users.views import UserLoginView
 
 urlpatterns = [
-    path('login/', UserLoginView.as_view(), name='login')
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
