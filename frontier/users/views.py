06a428fb666e84233fa5561635fa8f59bf68fd63
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-01 18:38:42 +0400

    Im about to change User Model

--- frontier/users/views.py
+++ frontier/users/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import CreateView
 
 from users.forms import UserLoginForm, UserCreationForm
 
