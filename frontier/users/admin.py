ef6554d61718808700b07f893d8588dee59cf3d2
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-06 18:04:33 +0400

    Added Profile model OTO User model

--- frontier/users/admin.py
+++ frontier/users/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from .models import *
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ['user', ('photo', 'show_photo'), 'bio']
+    list_display = ['user', 'show_photo']
+    readonly_fields = ['show_photo']
+    search_fields = ['user']
+
+    @admin.display(description='Фотография пользователя')
+    def show_photo(self, profile: Profile):
+        if profile.photo:
+            return mark_safe(f"<img src='{profile.photo.url}' width=50>")
+        return 'Нет изображения'
