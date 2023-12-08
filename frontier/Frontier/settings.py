1e891db5aa21a7715ccdbec7a235c5be414bf8c1
Alex Luenko <new.luenkoaleksei@gmail.com>
2023-12-08 18:10:00 +0400

    Impelemented email verification functionality.

--- frontier/Frontier/settings.py
+++ frontier/Frontier/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-DOMAIN_NAME = 'http://127.0.0.1:8000/'
+DOMAIN_NAME = 'http://127.0.0.1:8000'
 
 
 # Application definition
